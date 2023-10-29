76672fe1131f7117b4279db566b589798b343253
Andrey Agarkov <agarkov@peterpartner.net>
2023-10-29 08:25:21 +0300

    add module-manage interface

--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,13 @@
 from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Course
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
@@ -49,3 +51,35 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+    
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+                             data=data)
+    
+    def dispatch(self, request: HttpRequest, pk: Any) -> HttpResponse:
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super().dispatch(request, pk)
+    
+    def get(self, request: HttpRequest, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset
+        })
+    
+    def post(self, request: HttpRequest, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset
+        })
