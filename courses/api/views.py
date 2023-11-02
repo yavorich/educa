b6aa13bc3da3a207438efcac281234a37ba0c4b6
Andrey Agarkov <agarkov@peterpartner.net>
2023-11-02 07:01:21 +0300

    add full course view to api

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,7 +7,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer, \
+    CourseWithContentsSerializer
+from courses.api.permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -42,3 +44,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    # отображение курса вместе с содержимым
+    # только для зачисленных пользователей
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
