70f626cae269f665000958831d570c7e675a5268
Andrey Agarkov <agarkov@peterpartner.net>
2023-11-02 06:20:33 +0300

    add courses viewSet & router

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
 app_name = 'courses'
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 urlpatterns = [
     path('subjects/',
@@ -14,4 +18,5 @@ urlpatterns = [
     path('courses/<pk>/enroll/',
          views.CourseEnrollView.as_view(),
          name='course_enroll'),
+    path('', include(router.urls)),
 ]
